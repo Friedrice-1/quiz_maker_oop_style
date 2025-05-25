7eba2056b8dbe3385c86967e4caed2560d788d8f
Friedrice-1 <allenjayansing99@gmail.com>
2025-05-25 11:14:13 +0800

    Pseudocode

---
+++ quiz_main.py
@@ -0,0 +1,7 @@
+# Import the QuestionManager class from question_manager module
+# Import the QuizLoader class from quiz_loader module
+# Create a function for a menu
+# Ask user what they want to do
+# Load question manager
+# Load quiz loader
+# Check if this file is being run directly

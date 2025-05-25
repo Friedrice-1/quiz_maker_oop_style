f2ed9315ce0efc8a6b793c7f9ea86b6980b608ca
Friedrice-1 <allenjayansing99@gmail.com>
2025-05-25 10:21:39 +0800

    Pseudocode

---
+++ question_manager.py
@@ -0,0 +1,5 @@
+# Create a class for question manager
+# Initialize the class with an empty list to store questions
+# Loop to add questions through user input
+# Create a dictionary for the question and add it to the list
+# Save created questions to a text file

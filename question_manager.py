3038126e3b8c19f9c1e4052550dedfecbccbe3b5
Friedrice-1 <allenjayansing99@gmail.com>
2025-05-25 10:28:20 +0800

    Created a def function to store the loop for questions

--- question_manager.py
+++ question_manager.py
@@ -4,5 +4,6 @@ class QuestionManager:
     def __init__(self):
         self.questions = []
 # Loop to add questions through user input
+    def add_question(self):
 # Create a dictionary for the question and add it to the list
 # Save created questions to a text file

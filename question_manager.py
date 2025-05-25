6f3e62f5b331e2420f13e0ea524e75259a4c5300
Friedrice-1 <allenjayansing99@gmail.com>
2025-05-25 10:25:38 +0800

    Created a class with an empty list to store questions

--- question_manager.py
+++ question_manager.py
@@ -1,5 +1,8 @@
 # Create a class for question manager
+class QuestionManager:
 # Initialize the class with an empty list to store questions
+    def __init__(self):
+        self.questions = []
 # Loop to add questions through user input
 # Create a dictionary for the question and add it to the list
 # Save created questions to a text file

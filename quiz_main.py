841eb3ac5f8621230873ee854e704b1c9d356779
Friedrice-1 <allenjayansing99@gmail.com>
2025-05-25 11:17:01 +0800

    Import classes from modules

--- quiz_main.py
+++ quiz_main.py
@@ -1,5 +1,7 @@
 # Import the QuestionManager class from question_manager module
+from question_manager import QuestionManager
 # Import the QuizLoader class from quiz_loader module
+from quiz_loader import QuizLoader
 # Create a function for a menu
 # Ask user what they want to do
 # Load question manager
